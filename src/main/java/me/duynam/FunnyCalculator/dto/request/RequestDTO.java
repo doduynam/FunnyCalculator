7477ce8326bc63ae46c339216ff26180e68e57d0
doduynam <39905671+doduynam@users.noreply.github.com>
2023-02-12 18:48:34 +0700

    do: add dto and controller add number

---
+++ src/main/java/me/duynam/FunnyCalculator/dto/request/RequestDTO.java
@@ -0,0 +1,11 @@
+package me.duynam.FunnyCalculator.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class RequestDTO {
+	private String numOne;
+	private String numTwo;
+}
