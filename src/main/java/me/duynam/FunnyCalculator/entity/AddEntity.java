7599c6d96c90eae91193ed2ebd45d328ad680319
doduynam <39905671+doduynam@users.noreply.github.com>
2023-02-12 23:24:48 +0700

    do: add generate value for id

--- src/main/java/me/duynam/FunnyCalculator/entity/AddEntity.java
+++ src/main/java/me/duynam/FunnyCalculator/entity/AddEntity.java
@@ -2,6 +2,8 @@ package me.duynam.FunnyCalculator.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Data;
@@ -14,6 +16,7 @@ import lombok.NoArgsConstructor;
 public class AddEntity {
 
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "id")
 	private Long id;
 
